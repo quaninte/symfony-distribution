061accb6ee2122714086e835fd5ff5681859c9ff
Joshua Estes <Joshua.Estes@iostudio.com>
2012-07-13 07:43:14 -0500

    enabled iostudioFrameworkBundle and updated the composer lock file

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new iostudio\FrameworkBundle\iostudioFrameworkBundle(),
             new iostudio\DefaultBundle\iostudioDefaultBundle(),
         );
 
