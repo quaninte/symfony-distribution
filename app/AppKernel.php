4ef6f8102a3ec5b1a064ce5fcdfe04414005c4ca
Joshua Estes <Joshua.Estes@iostudio.com>
2012-07-31 13:32:50 -0500

    enabled app kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new iostudio\FrameworkBundle\iostudioFrameworkBundle(),
+            new iostudio\GoogleAnalyticsBundle\iostudioGoogleAnalyticsBundle(),
             new iostudio\DefaultBundle\iostudioDefaultBundle(),
         );
 
