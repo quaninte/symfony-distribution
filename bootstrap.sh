ac2b72124671a1d12f0a2167670c8dd7397a1691
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2013-03-06 13:51:51 -0600

    added bootstrap file

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+####
+#
+# bootstrap.sh - This file is used to bootstrap your project.
+# Once you clone this project, you should run this script and
+# it will set up your database and anything else in the project
+#
+
